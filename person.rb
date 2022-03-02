09a8e52a840c9cb0074e1e512cbbc50f50f1aaf6
emmanuel <gbengacode>
2022-03-02 13:03:29 +0100

    Add relationship between student and rental

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 require_relative('./corrector')
+require_relative('./rental')
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rental
 
   def initialize(age, name = 'unknown', parent_permision: true)
     @id = Random.rand(1..1000)
@@ -9,6 +10,7 @@ class Person
     @age = age
     @parent_permision = parent_permision
     @corrector = Corrector.new
+    @rental = []
   end
 
   def of_age?
@@ -21,6 +23,10 @@ class Person
 
   def validate_name
     @name = @corrector.correct_name(@name)
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
   private :of_age?
 end
